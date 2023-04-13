27951343924731e27fa76ea1ecdf871cb8566c16
Federico Ottomano <58518844+FedeOtto@users.noreply.github.com>
2023-04-13 15:35:46 +0100

    Update data.py

--- data.py
+++ data.py
@@ -7,15 +7,11 @@ import numpy as np
 import torch
 
 class binded_dataset(Dataset):
-    
     def __init__(self, df):
-                
         self.ratios = df.values
-        
         self.masks = np.where(self.ratios > 0, 1, 0.0)
         
     def __getitem__(self, idx):
-        
         return (torch.tensor(self.masks[idx], dtype=torch.float32),
                torch.tensor(self.ratios[idx], dtype=torch.float32))
     
